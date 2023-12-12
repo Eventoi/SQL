872a0704a58a8b4fceb176dbe5e8493464454763
Eventoi <Githubt3st@mail.ru>
2023-12-12 11:13:24 +0300

    Продвинутая выборка данных

--- MusicalService (SELECT).sql
+++ MusicalService (SELECT).sql
@@ -1,8 +1,10 @@
 -- Название и продолжительность самого длительного трека
 SELECT name, duration
 FROM tracks
-ORDER BY duration DESC
-LIMIT 1
+WHERE duration = (
+    SELECT MAX(duration)
+    FROM tracks
+    )
 
 -- Название треков, продолжительность которых не менее 3,5 минут
 SELECT name
@@ -22,7 +24,7 @@ WHERE name NOT LIKE '% %'
 -- Название треков, которые содержат слово «мой» или «my»
 SELECT name
 FROM tracks
-WHERE name LIKE '%My%'
+WHERE name iLIKE '%my%'
 
 -- Количество исполнителей в каждом жанре
 SELECT g.name AS genre, COUNT(a.id) AS count
@@ -35,29 +37,32 @@ ORDER BY count DESC
 -- Количество треков, вошедших в альбомы 2019–2020 годов
 SELECT COUNT(t.id) AS count
 FROM tracks AS t
-INNER JOIN albums AS a ON t.album_id = a.id
+JOIN albums AS a ON t.album_id = a.id
 WHERE a.release_year BETWEEN 2019 AND 2020
 
 -- Средняя продолжительность треков по каждому альбому
 SELECT a.name AS album, AVG(t.duration) AS average_duration
 FROM albums AS a
-INNER JOIN tracks AS t ON a.id = t.album_id
+JOIN tracks AS t ON a.id = t.album_id
 GROUP BY a.name
 
 -- Все исполнители, которые не выпустили альбомы в 2020 году
-SELECT a.name
-FROM artists AS a
-LEFT JOIN artists_albums AS aa ON a.id = aa.artist_id
-LEFT JOIN albums AS al ON aa.album_id = al.id
-WHERE al.release_year <> 2020 OR al.release_year IS NULL
+SELECT name
+FROM artists
+WHERE id NOT IN (
+    SELECT DISTINCT artist_id
+    FROM artists_albums AS aa
+    JOIN albums AS a ON aa.album_id = a.id
+    WHERE a.release_year = 2020
+    )
 
 -- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами)
 SELECT c.name
 FROM compilations AS c
-INNER JOIN tracks_compilations AS tc ON c.id = tc.compilation_id
-INNER JOIN tracks AS t ON tc.track_id = t.id
-INNER JOIN artists_albums AS aa ON t.album_id = aa.album_id
-INNER JOIN artists AS a ON aa.artist_id = a.id
+JOIN tracks_compilations AS tc ON c.id = tc.compilation_id
+JOIN tracks AS t ON tc.track_id = t.id
+JOIN artists_albums AS aa ON t.album_id = aa.album_id
+JOIN artists AS a ON aa.artist_id = a.id
 WHERE a.name = 'Parkway Drive'
 
 -- Названия альбомов, в которых присутствуют исполнители более чем одного жанра
@@ -85,18 +90,15 @@ WHERE t.duration = (
 	)
 
 -- Названия альбомов, содержащих наименьшее количество треков
-SELECT a.name
+SELECT a.name, COUNT(t.name) track_count
 FROM albums AS a
-JOIN (
-    SELECT album_id, COUNT(*) AS track_count
-    FROM tracks AS t
-    GROUP BY album_id
-) AS track_counts ON a.id = track_counts.album_id
-WHERE track_counts.track_count = (
-    SELECT MIN(track_count)
-    FROM (
-        SELECT COUNT(*) AS track_count
-        FROM tracks AS t
-        GROUP BY album_id
-    ) AS subquery
-)
+JOIN tracks AS t ON a.id = t.album_id
+GROUP BY a.id
+HAVING COUNT(t.name) = (  
+	SELECT COUNT(t.name)
+	FROM albums AS a
+	JOIN tracks AS t ON a.id = t.id
+	GROUP BY a.id
+	ORDER BY COUNT(t.name)
+	LIMIT 1
+	)
